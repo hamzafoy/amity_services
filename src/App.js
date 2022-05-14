2b602f2f0822a73bd4f514a472449edd02e3c4a0
hamzafoy <hamzaoflouisville@gmail.com>
2022-05-14 00:41:46 -0400

    constructed the BrowserRouter and all subpage routes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
+/*::::::::::::::::::::::::::::::::::::::::
+:  Importing CSS, Dependencies, Routes  ::
+::::::::::::::::::::::::::::::::::::::::*/
+
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Accounting from './components/Accounting/Accounting';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home';
+import Other from './components/Other/Other';
+import Paralegal from './components/Paralegal/Paralegal';
+import Travel from './components/Travel/Travel';
+
+
+
+/*::::::::::::::::::::::::::::::::::::::::
+:::::::  Creating App Component  :::::::::
+::::::::::::::::::::::::::::::::::::::::*/
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter>
+      
+        <Routes>
+
+          <Route 
+          path="/" 
+          element={<Home/>}
+          />
+
+          <Route 
+          path="/accounting" 
+          element={<Accounting/>}
+          />
+
+          <Route
+          path="/contact"
+          element={<Contact/>}
+          />
+
+          <Route
+          path="/additional_services"
+          element={<Other/>}
+          />
+
+          <Route
+          path="/paralegal"
+          element={<Paralegal/>}
+          />
+
+          <Route
+          path="/travel"
+          element={<Travel/>}
+          />
+
+        </Routes>
+
+      </BrowserRouter>
+
     </div>
   );
 }
+
+
+
+/*::::::::::::::::::::::::::::::::::::::::
+::::::  Exporting App Component  :::::::::
+::::::::::::::::::::::::::::::::::::::::*/
 
 export default App;
