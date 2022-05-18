ee51aadf744ef8caf23efd488e67d98652bbe6e6
hamzafoy <hamzaoflouisville@gmail.com>
2022-05-17 20:52:24 -0400

    building out navbar.

---
+++ src/components/Home/Home.js
@@ -0,0 +1,37 @@
+/*::::::::::::::::::::::::::::::::::::::::
+:  Importing CSS, Dependencies, Routes  ::
+::::::::::::::::::::::::::::::::::::::::*/
+
+import './Home.css';
+import NavBar from '../subcomponents/NavBar/NavBar';
+
+
+
+/*::::::::::::::::::::::::::::::::::::::::
+:::::::  Creating App Component  :::::::::
+::::::::::::::::::::::::::::::::::::::::*/
+
+function App() {
+  return (
+
+    <>
+
+        <NavBar/>
+
+        <div className="homePage">
+        
+        
+        </div>
+
+    </>
+
+  );
+}
+
+
+
+/*::::::::::::::::::::::::::::::::::::::::
+::::::  Exporting App Component  :::::::::
+::::::::::::::::::::::::::::::::::::::::*/
+
+export default App;
---
+++ src/components/subcomponents/NavBar/NavBar.js
@@ -0,0 +1,51 @@
+/*::::::::::::::::::::::::::::::::::::::::
+:  Importing CSS, Dependencies, Routes  ::
+::::::::::::::::::::::::::::::::::::::::*/
+
+import './NavBar.css';
+
+
+
+/*::::::::::::::::::::::::::::::::::::::::
+:::::::  Creating App Component  :::::::::
+::::::::::::::::::::::::::::::::::::::::*/
+
+function NavBar() {
+  return (
+    
+    <nav className="navigationBar">
+
+        <img
+        alt="Amity Services logo of an airliner at the end of a flourished letter Y."
+        className="navigationBar_logo"
+        src="https://via.placeholder.com/450x100?text=Amity+Services+Logo+goes+here"
+        />
+
+        <aside className="navigationBar_menu">
+
+            <h1>
+                Accounting
+            </h1>
+
+            <h1>
+                Paralegal
+            </h1>
+
+            <h1>
+                Travel
+            </h1>
+
+        </aside>
+
+    </nav>
+
+  );
+}
+
+
+
+/*::::::::::::::::::::::::::::::::::::::::
+::::::  Exporting App Component  :::::::::
+::::::::::::::::::::::::::::::::::::::::*/
+
+export default NavBar;
