364e564d48a69e0ff2298919d8a00894c3348daf
hamzafoy <hamzaoflouisville@gmail.com>
2022-06-08 14:17:10 -0400

    adding content to subpages.

--- src/components/Accounting/Accounting.js
+++ src/components/Accounting/Accounting.js
@@ -20,6 +20,22 @@ function Accounting() {
         <NavBar/>
 
         <div className="accountingPage">
+
+          <section className="homePage_info">
+
+            <h1>
+              Our Accounting Services
+            </h1>
+
+            <p>
+              Amity Services LLC provides tax filing assistance including preparation for both 
+              individuals and businesses. We have the means to provide translation for Arabic &amp;
+              French speakers to aid them in filing taxes within the federal deadline. Ou quick &amp;
+              convenient service paired with excellent customer service will leave you feeling 
+              satisfied that you are in compliance with federal and state taxes!
+            </p>
+
+          </section>
         
         
         </div>
--- src/components/Paralegal/Paralegal.js
+++ src/components/Paralegal/Paralegal.js
@@ -20,7 +20,23 @@ function Paralegal() {
         <NavBar/>
 
         <div className="paralegalPage">
-        
+
+          <section className="homePage_info">
+
+            <h1>
+              Our Paralegal Services
+            </h1>
+
+            <p>
+              Amity Services LLC provides a number of services including public notary, 
+              certified translations in Arabic, English, &amp; French, and legal document 
+              preparation. Our in-house paralegal specializes in immigration work and can 
+              help prepare forms and documents including those related to asylum, citizenship, 
+              green card, petitions for relatives, TPS, visas (investor, lottery, student), 
+              work authorization. We can also help with contract preparation for businesses.
+            </p>
+
+          </section>
         
         </div>
 
