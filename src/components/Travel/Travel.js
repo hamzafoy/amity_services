783926a660f9c01a7456aa76f12f2cbd5c934caf
hamzafoy <hamzaoflouisville@gmail.com>
2022-06-08 17:45:10 -0400

    adding content to Travel subpage.

--- src/components/Travel/Travel.js
+++ src/components/Travel/Travel.js
@@ -20,7 +20,23 @@ function Travel() {
         <NavBar/>
 
         <div className="travelPage">
-        
+
+          <section className="homePage_info">
+
+            <h1>
+              Our Travel Services
+            </h1>
+
+            <p>
+              Amity Services LLC assists clients with ticketing services for both 
+              domestic &amp; international flights. Amity can also assist with arranging 
+              visas to Saudi Arabia &amp; other countries. Our company prepare vacation 
+              packages to locations including Africa, Cancun, Europe, Hawaii, the Middle 
+              East, &amp; Turkey. We also prepare Hajj &amp; Umrah packages for your 
+              convenience!
+            </p>
+
+          </section>
         
         </div>
 
