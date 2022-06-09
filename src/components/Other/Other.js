8ab1f3bbb60f05e35062860d98b5a44baa4f5be9
hamzafoy <hamzaoflouisville@gmail.com>
2022-06-09 16:43:53 -0400

    saving content for Property Mgmt subpage.

--- src/components/Other/Other.js
+++ src/components/Other/Other.js
@@ -21,6 +21,21 @@ function Other() {
 
         <div className="otherPage">
         
+          <section className="homePage_info">
+
+            <h1>
+              Property Management Services
+            </h1>
+
+            <p>
+              Amity Services LLC helps those looking to find apartments to 
+              rent or houses to purchase. We accept Section 8 vouchers and 
+              can help those trying to secure homes. We have a real estate 
+              agent as part of our team. Our services also include providing 
+              assistance for those looking to renovate properties.
+            </p>
+
+          </section>
         
         </div>
 
