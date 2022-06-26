f2c038ac50475fc8a38aa08456b06e53f9e95792
hamzafoy <hamzaoflouisville@gmail.com>
2022-06-25 23:18:36 -0400

    added Umrah/Hajj section to travel subpage.

--- src/components/Travel/Travel.js
+++ src/components/Travel/Travel.js
@@ -9,6 +9,7 @@ import africa from './resources/kenya.jpeg';
 import cancun from './resources/cancun.jpg';
 import europe from './resources/europe.jpg';
 import hawaii from './resources/hawaii.jpg';
+import imageFlyer from './resources/image.png';
 import mideast from './resources/amman.jpg';
 import turkey from './resources/turkey.jpg';
 
@@ -141,6 +142,49 @@ function Travel() {
 
             </section>
           </section>
+
+          <section className="travelPage_info">
+
+              <h1>
+                Our Hajj &amp; Umrah Packages
+              </h1>
+
+              <aside className="travelPage_options">
+
+                <article className="travelPage_options__card">
+
+                  <h1>
+                  Umrah
+                  </h1>
+
+                  <h2>
+                    Dec. 27th 2022 - Jan. 3rd 2023
+                    <br/>
+                    Single: $3,395, Double: $2,895
+                  </h2>
+
+                  <img
+                  alt="Image of Nairobi."
+                  className="travelPage_card__foto"
+                  src={africa}
+                  />
+
+                  <p>
+                    Visa processing for American passports only, <br/>
+                    5-star hotel facing the Haram in Mecca &amp; Madina, <br/>
+                    Daily open buffet breakfast, <br/>
+                    Visit to Mazarat in Mecca &amp; Medina, <br/>
+                    Amity Travel supporting staff 24×7 <br/>
+                    PCR testing in Saudi Arabia on return, <br/>
+                    Does not include domestic add-on <br/> luggage fees to &amp; from the gateways. <br/>
+                    <a href={imageFlyer} target="_blank" download={imageFlyer}>More details here </a>
+                  </p>
+
+                </article>
+
+              </aside>
+
+            </section>
         
         </div>
 
